9a81d6c5a32ee0e0f53054940a08ba9b298785f1
dimashky <13630469+dimashky@users.noreply.github.com>
2025-09-09 22:35:34 +0300

    remove helloworld and add version to config

--- app/handlers/handlers.go
+++ app/handlers/handlers.go
@@ -3,14 +3,12 @@ package handlers
 import (
 	"github.com/CodeLieutenant/uberfx-common/v3/http/fiber/fiberfx"
 
-	"github.com/dimashky/go-appointment/app/handlers/helloworld"
 	"github.com/dimashky/go-appointment/app/handlers/users"
 )
 
 func Handlers() fiberfx.RoutesFx {
 	return fiberfx.Routes(
 		[]fiberfx.RouteFx{
-			fiberfx.Get("/", helloworld.HelloWorld),
 			fiberfx.Post("/api/users", users.CreateUserRoute),
 			fiberfx.Get("/api/users/:id", users.GetUserRoute),
 			fiberfx.Get("/api/users", users.ListUsersRoute),
